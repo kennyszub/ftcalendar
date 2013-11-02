f42b2122df3cc059371c4c47e67f487a5b5405cc
saasbook <saasbook@saasbook.(none)>
2013-11-02 15:22:00 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ftcalendar::Application.config.secret_token = '118a539bbd61dd7edb579efea2655cda1ba66f064b04326bbe88448684190e1c8697def54c6abed6a92eae8f510b9ffedd1dd1b8de0283d9528cd502db0a7fc9'
