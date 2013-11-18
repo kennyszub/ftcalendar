66926e4c86442ef43629dad3bf08cda83eec75bc
saasbook <k.szubzda@berkeley.edu>
2013-11-17 22:10:19 -0800

    moved js into view

--- config/application.rb
+++ config/application.rb
@@ -40,9 +40,10 @@ module Ftcalendar
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8" 
 
+    # TODO removed?
     config.assets.initialize_on_precompile = false
 
-    config.assets.precompile = ['rails.validations.js', 'application.css']
+    config.assets.precompile += ['rails.validations.js', 'application.css']
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
